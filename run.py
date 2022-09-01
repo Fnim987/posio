2d1809000085407a4399f040d3324d4af181f859
EC2 Default User <ec2-user@ip-172-31-78-49.ec2.internal>
2022-09-01 07:36:31 +0000

    update config.yml and run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import socketio, app
+from flask.logging import create_logger
+
+LOG = create_logger(app)
 
 
 HOST = app.config.get('HOST', '127.0.0.1')
 PORT = app.config.get('PORT', 5000)
 
-app.logger.info('Starting server on {host}:{port}'.format(host=HOST, port=PORT))
+LOG.info('Starting server on {host}:{port}'.format(host=HOST, port=PORT))
 
 socketio.run(app, host=HOST, port=PORT)
