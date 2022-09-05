ce23d28be427ca8b80c777adb6eb51b258dabcb0
EC2 Default User <ec2-user@ip-172-31-78-49.ec2.internal>
2022-09-05 11:25:05 +0000

    test config.yml

--- ansible/roles/deploy/files/kube.sh
+++ ansible/roles/deploy/files/kube.sh
@@ -1,3 +1,3 @@
 kubectl apply -f kubedeploy.yaml
 sleep 15
-nohup kubectl port-forward deploy/posio 8000:5000 --address='0.0.0.0' > nohup.out 2>&1 &
+kubectl port-forward deploy/posio 8000:5000 --address='0.0.0.0' &
